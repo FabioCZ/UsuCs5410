a46deed8ca28a0792e6ce92e2e1244b0befd0b59
Fabio Gottlicher <fabiog@ifit.com>
2016-02-15 13:28:49 -0700

    one problem sovled, but now it gets stuck

--- HW2/maze.js
+++ HW2/maze.js
@@ -9,6 +9,7 @@ MazeGame.mazeArray = (function () {
     'use strict';
 
     var mazeArray;
+    var size;
 
     var CellType = {
         EMPTY: 1,
@@ -21,23 +22,29 @@ MazeGame.mazeArray = (function () {
     }
 
     function init(size) {
+        console.log('start1');
+        
+        size = size;
         mazeArray = new Array(size)
         for (var i = 0; i < size; i++) {
             mazeArray[i] = new Array(size);
-            for (var j = 0; j < size; i++) {
+            for (var j = 0; j < size; j++) {
                 mazeArray[i][j] = CellType.EMPTY;
             }
         }
+        console.log('start2');
+
         recursiveDivider(0, 0, size, size);
 
     }
 
     function recursiveDivider(startX, startY, width, height) {
         if (width == 2 && height == 2) return;
-        
+        console.log('start3');
         //add vertical
         var verticalDivide = randomBetween(startX + 1, startX + width)
         for(var  j = startY; j < startY + height;j++){
+            console.log(j);
             mazeArray[verticalDivide][j] = CellType.WALL;
         }
         //add hole to vertical
@@ -59,6 +66,7 @@ MazeGame.mazeArray = (function () {
         var horizontalHole = randomBetween(startX, startX + width);
         mazeArray[horizontalHole][horizontalDivide] = CellType.EMPTY;
         
+        logMaze();
         //topleft
         recursiveDivider(startX, startY, verticalDivide - startX, horizontalDivide - startY);
         //topright
@@ -68,12 +76,23 @@ MazeGame.mazeArray = (function () {
         //bottomright
         recursiveDivider(verticalDivide, horizontalDivide, startX + width - verticalDivide, startY + height - horizontalDivide);
     }
+    
+    function logMaze(){
+        var a = '';
+        for(var j = 0; j < size;j++){
+            for(var i = 0; i < size; i++){
+                a += mazeArray[i][j];
+            }
+            a += '\\r\\n';
+        }
+        console.log(a);
+    }
 
 
     return {
         CellType: CellType,
         init: init,
-
+        logMaze : logMaze
     }
 } ());
 
