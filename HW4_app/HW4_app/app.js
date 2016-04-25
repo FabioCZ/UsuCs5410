e2e07ef008e786e22297ee03e22422e7ed5c266c
Fabio Gottlicher <g.fabio.cz@gmail.com>
2016-04-24 19:57:18 -0600

    naming fix

--- HW4_app/HW4_app/app.js
+++ HW4_app/HW4_app/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
-var bodyParser = require('body-parser');
 var fs = require('fs');
 app.use(bodyParser.json());
 
